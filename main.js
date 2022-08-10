a822456636d0078573f37d06ed7d9d8bb4e6af0e
sebastian <f.sebastianbarajas@gmail.com>
2022-08-10 12:17:02 -0500

    impementacion menu mobile

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
 const navBar = document.querySelector('.navbar-email');
 const deskMenu = document.querySelector('.desktop-menu');
 
+const mobileMenu = document.querySelector('.mobile-menu');
+const btnMenuMobile = document.querySelector('.menu');
+
 navBar.addEventListener('click',toggleDeskMenu);
+btnMenuMobile.addEventListener('click', toggleMovileMenu);
 
 
 function toggleDeskMenu(){
     deskMenu.classList.toggle('inactive');
+}
+
+function toggleMovileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
